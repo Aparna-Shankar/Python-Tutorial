78b82aa13e2ec61b3c0ca42fa0bb109df1f6930f
Aparna Shankar <aparna121@gmail.com>
2018-12-25 12:06:20 +0530

    Updated Loops and Iterations

--- Loops and Iterations.py
+++ Loops and Iterations.py
@@ -1,13 +1,13 @@
 
 # For Loop
 
-# nums = [1,2,3,4,5]
-# for num in nums:
-#   if num == 3:
-#       print('Found!')
-#       # break  (break out of the loop)
-#       continue   # continue with next iteration of the loop
-#   print(num)
+nums = [1, 2, 3, 4, 5]
+for num in nums:
+    if num == 3:
+        print('Found!')
+        # break  (break out of the loop)
+        continue   # continue with next iteration of the loop
+    print(num)
 
 # Nested loops
 # nums = [1,2,3,4,5]
@@ -17,14 +17,14 @@
 
 
 # Range
-# for i in range(10):
-#   print(i)
+for i in range(10):
+  print(i)
 
 # While Loops
 # Ctrl + C - To break out of an infinite loop
-x = 0
-while True:
-    if x == 4:
-        break
-    print(x)
-    x+= 1
+# x = 0
+# while True:
+#     if x == 4:
+#         break
+#     print(x)
+#     x+= 1
